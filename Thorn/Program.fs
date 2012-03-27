d1a0c0e29fadd1a9a724e7175d4740e2ed5633b1
cameron taggart <cameron.taggart@fluke.com>
2012-03-26 17:07:53 -0700

    Bug of the Day: "TH".StartsWith "?" = true

---
+++ Thorn/Program.fs
@@ -0,0 +1,20 @@
+﻿
+namespace SystemTest
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open System
+
+[<TestClass>]
+type ThornTest() =
+
+    [<TestMethod>]
+    member x.TestDefault() =
+        Assert.IsFalse <| "TH".StartsWith "Þ" // fail
+
+    [<TestMethod>]
+    member x.TestCurrentCulture() =
+        Assert.IsFalse <| "TH".StartsWith("Þ", StringComparison.CurrentCulture) // fail
+
+    [<TestMethod>]
+    member x.TestOrdinal() =
+        Assert.IsFalse <| "TH".StartsWith("Þ", StringComparison.Ordinal) // pass
